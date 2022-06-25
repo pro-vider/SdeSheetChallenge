db175cd6b6536f95d3454ba60a49ba97826b5e75
Suprodip Kundu <suprodipkundu718@gmail.com>
2022-06-25 09:51:31 +0530

    Create maximumMeetings.cpp

---
+++ Greedy/maximumMeetings.cpp
@@ -0,0 +1,36 @@
+#Link-https://www.codingninjas.com/codestudio/problems/1062658?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+#include<bits/stdc++.h>
+struct meeting{
+    int start;
+    int end;
+    int pos;
+};
+bool comparator(struct meeting m1,meeting m2){
+    if(m1.end<m2.end) return true;
+    else if(m1.end>m2.end) return false;
+    else if(m1.pos<m2.pos) return true;
+    else return false;
+}
+vector<int> maximumMeetings(vector<int> &s, vector<int> &e) {
+    // Write your code here.
+    int n=s.size();
+    struct meeting meet[n];
+    for(int i=0;i<n;i++){
+        meet[i].start=s[i],meet[i].end=e[i],meet[i].pos=i+1;
+    }
+    sort(meet,meet+n,comparator);
+    vector<int> ans;
+    int ending=meet[0].end;
+    ans.push_back(meet[0].pos);
+       for(int i=1;i<n;i++){
+        if(meet[i].start>ending){
+            ending=meet[i].end;
+            ans.push_back(meet[i].pos);
+        }
+    }
+    return ans;
+}
+
+#Time Complexity-O(NlogN)
+#Space Complexity-O(N)
