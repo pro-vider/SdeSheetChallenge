89a8ed2c7b006bdc2cd15b26cc376392a3adcbcf
Suprodip Kundu <suprodipkundu718@gmail.com>
2022-07-06 18:43:44 +0530

    Create identicalTrees.cpp

---
+++ Binary Tree/identicalTrees.cpp
@@ -0,0 +1,27 @@
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+
+    class BinaryTreeNode {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+		
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+***********************************************************/
+
+bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+    // Write your code here. 	 
+    if(!root1 or !root2)
+        return(root1==root2);
+    return((root1->data==root2->data) and identicalTrees(root1->left,root2->left) and identicalTrees(root1->right,root2->right));
+}
