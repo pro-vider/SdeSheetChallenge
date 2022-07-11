f8aefe6afc531bfdfc361cd523c9c9eedfe7b11a
Suprodip Kundu <suprodipkundu718@gmail.com>
2022-07-11 10:38:30 +0530

    Update KthLargestNumber.cpp

--- Binary Search Tree/KthLargestNumber.cpp
+++ Binary Search Tree/KthLargestNumber.cpp
@@ -1,3 +1,18 @@
+/************************************************************
+    Following is the TreeNode class structure
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+    *********************************************************/
 void inorder(TreeNode<int>* root, int &k, int &ans)
 {
         if(root == NULL)
