4f30eb6fa8cb0b60ef24971c091fbe21a0da023d
Suprodip Kundu <suprodipkundu718@gmail.com>
2022-07-11 09:54:36 +0530

    Create deleteNodeInBST.cpp

---
+++ Binary Search Tree/deleteNodeInBST.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* findleftleave(TreeNode* root)
+    {
+        if(root->left==NULL)
+            return root;
+        return findleftleave(root->left);
+    }
+    TreeNode* solve(TreeNode* root)
+    {
+        if(root->left==NULL)
+            return root->right;
+        if(root->right==NULL)
+            return root->left;
+        TreeNode* leftside= root->left;
+        TreeNode* temp=findleftleave(root->right);
+        temp->left=leftside;
+        return root->right;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root)
+            return NULL;
+        if(root->val==key)
+           return solve(root);
+        TreeNode* ans=root;
+        while(root)
+        {
+            if(root->val>key)
+            {
+                if(root->left!=NULL and root->left->val==key)
+                {
+                    root->left=solve(root->left);
+                    break;
+                }
+                else
+                    root=root->left;
+            }
+            else
+            {
+                 if(root->right!=NULL and root->right->val==key)
+                {
+                    root->right=solve(root->right);
+                    break;
+                }
+                else
+                    root=root->right;
+            }
+        }
+        return ans;
+        
+    }
+};
