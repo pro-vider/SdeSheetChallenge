0e755932bd7c042fb920308725a4e9d021757a96
Suprodip Kundu <suprodipkundu718@gmail.com>
2022-07-28 11:16:33 +0530

    Create 01knapsack.cpp

---
+++ Dynamic Programming/01knapsack.cpp
@@ -0,0 +1,53 @@
+#tabulation
+
+int maxProfit(vector<int> &val, vector<int> &wt, int n, int W)
+{
+	// Write your code here
+    vector<vector<int>>dp(n,vector<int>(W+1,0));
+    for(int i=wt[0]; i<=W; i++){
+        dp[0][i] = val[0];
+    }
+    
+    for(int ind =1; ind<n; ind++){
+        for(int cap=0; cap<=W; cap++){
+            
+            int notTaken = 0 + dp[ind-1][cap];
+            
+            int taken = INT_MIN;
+            if(wt[ind] <= cap)
+                taken = val[ind] + dp[ind-1][cap - wt[ind]];
+                
+            dp[ind][cap] = max(notTaken, taken);
+        }
+    }
+    
+    return dp[n-1][W];
+}
+
+#space optimization
+
+int maxProfit(vector<int> &val, vector<int> &wt, int n, int W)
+{
+	// Write your code here
+    //vector<vector<int>>dp(n,vector<int>(W+1,0));
+    vector<int> prev(W+1,0),cur(W+1,0);
+    for(int i=wt[0]; i<=W; i++){
+        prev[i] = val[0];
+    }
+    
+    for(int ind =1; ind<n; ind++){
+        for(int cap=0; cap<=W; cap++){
+            
+            int notTaken = 0 + prev[cap];
+            
+            int taken = INT_MIN;
+            if(wt[ind] <= cap)
+                taken = val[ind] + prev[cap - wt[ind]];
+                
+            cur[cap] = max(notTaken, taken);
+        }
+        prev=cur;
+    }
+    
+    return prev[W];
+}
